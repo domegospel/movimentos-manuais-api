5c9409beaec520d8b2c0344fa3e614ad7a72829a
Ronaldo Freitas <ronaldo.freitas@gx2.com.br>
2026-07-03 12:20:51 -0300

    Initial

---
+++ src/main/java/br/com/antlia/movimentosmanuais/MovimentosManuaisApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.antlia.movimentosmanuais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovimentosManuaisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovimentosManuaisApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/antlia/movimentosmanuais/MovimentosManuaisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.antlia.movimentosmanuais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovimentosManuaisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
